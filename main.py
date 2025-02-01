00b83ef743d707b6862f2a8aa27303e4e5ab21fb
rishav_raj <145801777+rishav-wq@users.noreply.github.com>
2025-02-02 02:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,5 +70,5 @@ def index():
     return render_template('index.html', result=None)
 
 # Run Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10002)
